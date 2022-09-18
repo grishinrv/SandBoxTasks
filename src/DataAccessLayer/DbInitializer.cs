0a3cf59a82b9300af8c23067a2f485f829fbb962
RomanGrishin <roman1988grishin@gmail.com>
2022-09-18 15:37:22 +0300

    Add DataAccessLayer project

---
+++ src/DataAccessLayer/DbInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLayer;
+
+public sealed class DbInitializer
+{
+    private readonly string _connectionString;
+    
+    public DbInitializer(IConfiguration config)
+    {
+        _connectionString = config.GetConnectionString("StatisticsDbConnectionString");
+    }
+
+    public void Init()
+    {
+        
+    }
+}
