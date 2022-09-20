eb1ff1bcebd7caf20705146eb202b7b621e245b8
RomanGrishin <roman1988grishin@gmail.com>
2022-09-20 21:13:27 +0300

    Sql Command Parameters fill

--- src/DataAccessLayer/RawSqlPriceRepository.cs
+++ src/DataAccessLayer/RawSqlPriceRepository.cs
@@ -57,27 +57,13 @@ public class RawSqlPriceRepository : IPriceRepository
         {
             try
             {
-                SqlCommand command = new SqlCommand(@"SELECT * FROM [statistics].prices WHERE registered_time >= @since AND 
-                                                    registered_time < @till AND city_of_registration IN ('Podolsk', 'Moscow')", connection);
-                command.Parameters.AddWithValue("@since", since);
-                command.Parameters.AddWithValue("@till", till);
+                string sql = BuildSqlCommandText(citiesOfPriceRegistration);
+
+                SqlCommand command = new SqlCommand(sql, connection);
+                FillSqlParameters(since, till, citiesOfPriceRegistration, command);
+                
                 command.Connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                List<PriceData> results = new();
-                while (reader.Read())
-                {
-                    DateTime time = (DateTime)reader["registered_time"];
-                    string city = (string)reader["city_of_registration"];
-                    decimal val = (decimal)reader["value"];
-                    Good good = (Good)(int)reader["good_id"];
-                    results.Add(new PriceData
-                    {
-                        RegisteredTime = time,
-                        Value = val,
-                        CityOfRegistration = city,
-                        Good = good
-                    });
-                }
+                var results = GetResultsByCitiesAndDateTimePeriod(command);
                 return results;
             }
             catch (Exception e)
@@ -85,6 +71,57 @@ public class RawSqlPriceRepository : IPriceRepository
                 Console.WriteLine(e);
                 throw;
             }
+        }
+    }
+
+    private List<PriceData> GetResultsByCitiesAndDateTimePeriod(SqlCommand command)
+    {
+        SqlDataReader reader = command.ExecuteReader();
+        List<PriceData> results = new();
+        while (reader.Read())
+        {
+            DateTime time = (DateTime)reader["registered_time"];
+            string city = (string)reader["city_of_registration"];
+            decimal val = (decimal)reader["value"];
+            Good good = (Good)(int)reader["good_id"];
+            results.Add(new PriceData
+            {
+                RegisteredTime = time,
+                Value = val,
+                CityOfRegistration = city,
+                Good = good
+            });
+        }
+
+        return results;
+    }
+
+    private static string BuildSqlCommandText(List<string> citiesOfPriceRegistration)
+    {
+        string sql = "SELECT * FROM [statistics].prices WHERE registered_time >= @since AND registered_time < @till";
+        if (citiesOfPriceRegistration != null)
+        {
+            sql += " AND city_of_registration IN (";
+            for (int i = 0; i < citiesOfPriceRegistration.Count; i++)
+            {
+                if (i > 0)
+                    sql += ", ";
+                sql += "@city" + i;
+            }
+
+            sql += ")";
+        }
+
+        return sql;
+    }
+
+    private void FillSqlParameters(DateTime since, DateTime till, List<string> citiesOfPriceRegistration, SqlCommand command)
+    {
+        command.Parameters.AddWithValue("@since", since);
+        command.Parameters.AddWithValue("@till", till);
+        for (int i = 0; i < citiesOfPriceRegistration.Count; i++)
+        {
+            command.Parameters.AddWithValue("@city" + i, citiesOfPriceRegistration[i]);
         }
     }
 
