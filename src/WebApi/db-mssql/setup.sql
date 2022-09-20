2e25c8a33b7ae6cb9dafd2999f119ab7e7296e6a
RomanGrishin <roman1988grishin@gmail.com>
2022-09-20 19:05:47 +0300

    custom db docker container with db initialisation support

---
+++ src/WebApi/db-mssql/setup.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE $(MSSQL_DB);
+GO
+USE $(MSSQL_DB);
+GO
+CREATE LOGIN $(MSSQL_USER) WITH PASSWORD = '$(MSSQL_PASSWORD)';
+GO
+CREATE USER $(MSSQL_USER) FOR LOGIN $(MSSQL_USER);
+GO
+ALTER SERVER ROLE sysadmin ADD MEMBER [$(MSSQL_USER)];
+GO
+
+USE price_stat;
+GO
+CREATE SCHEMA [statistics];
+GO
+CREATE TABLE [statistics].[prices] (
+    registered_time Datetime NOT NULL,
+    [value] DECIMAL NOT NULL,
+    city_of_registration NVARCHAR(64) NOT NULL,
+    good_id int NOT NULL
+);
+GO
+
