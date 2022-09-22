9436747b929ce38de12b4945b1f6dfc34dfd7536
RomanGrishin <roman1988grishin@gmail.com>
2022-09-22 20:00:17 +0300

    parse env

--- src/WebApi/db-mssql/import-data.sh
+++ src/WebApi/db-mssql/import-data.sh
@@ -1,11 +1,15 @@
 #run the setup script to create the DB and the schema in the DB
+echo "Parsing setup.sql environment variables..."
+envsubst < setup.sql > setup_parsed.sql
+echo "setup.sql parsed. Executing..."
+
 #do this in a loop because the timing for when the SQL instance is ready is indeterminate
 for i in {1..50};
 do
-    /opt/mssql-tools18/bin/sqlcmd -S localhost -C -U sa -P $(eval echo \$\{MSSQL_SA_PASSWORD\}) -i setup.sql
+    /opt/mssql-tools18/bin/sqlcmd -S localhost -C -U sa -P $(eval echo \$\{MSSQL_SA_PASSWORD\}) -i setup_parsed.sql
     if [ $? -eq 0 ]
     then
-        echo "setup.sql completed"
+        echo "DB setup completed"
         break
     else
         echo "not ready yet..."
