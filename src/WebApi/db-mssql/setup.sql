436a6e84da7d82193124d87b4c2cd4d7173f5eae
RomanGrishin <roman1988grishin@gmail.com>
2022-09-22 20:00:26 +0300

    setup (in progress)

--- src/WebApi/db-mssql/setup.sql
+++ src/WebApi/db-mssql/setup.sql
@@ -1,32 +1,32 @@
-CREATE DATABASE $(MSSQL_DB);
-GO
-USE $(MSSQL_DB);
-GO
-CREATE LOGIN $(MSSQL_USER) WITH PASSWORD = '$(MSSQL_PASSWORD)';
-GO
-CREATE USER $(MSSQL_USER) FOR LOGIN $(MSSQL_USER);
-GO
-ALTER SERVER ROLE sysadmin ADD MEMBER [$(MSSQL_USER)];
+IF DB_ID ('$MSSQL_DB') IS NULL
+BEGIN
+  CREATE DATABASE $MSSQL_DB;
+END
 GO
 
-USE price_stat;
-GO
-CREATE SCHEMA [statistics];
-GO
-CREATE TABLE [statistics].[prices] (
-    registered_time Datetime NOT NULL,
-    [value] DECIMAL NOT NULL,
-    city_of_registration NVARCHAR(64) NOT NULL,
-    good_id int NOT NULL
-);
-GO
+USE $MSSQL_DB;
+IF NOT EXISTS 
+BEGIN
+  CREATE LOGIN [$MSSQL_USER] WITH PASSWORD = '$MSSQL_PASSWORD';
+  CREATE USER [$MSSQL_USER] FOR LOGIN [$MSSQL_USER];
+  ALTER SERVER ROLE sysadmin ADD MEMBER [$MSSQL_USER];
 
-CREATE TABLE [statistics].[goods_dictionary] (
-    [id] INT NOT NULL,
-    [Name] NVARCHAR(64) NOT NULL
-);
-GO
-CREATE UNIQUE INDEX [statistics].UI_goods_dictionary_name
-    ON [statistics].[goods_dictionary] (Name);
-GO
+  CREATE SCHEMA [statistics];
+
+  CREATE TABLE [statistics].[prices] (
+      registered_time Datetime NOT NULL,
+      [value] DECIMAL NOT NULL,
+      city_of_registration NVARCHAR(64) NOT NULL,
+      good_id int NOT NULL
+  );
+
+  CREATE TABLE [statistics].[goods_dictionary] (
+     [id] INT NOT NULL,
+     [Name] NVARCHAR(64) NOT NULL
+  );
+
+  CREATE UNIQUE INDEX [statistics].UI_goods_dictionary_name
+     ON [statistics].[goods_dictionary] (Name);
+
+END
 
