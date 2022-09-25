1b44d4f0af663c3fa88218c76fa52dda4e2f10b7
RomanGrishin <roman1988grishin@gmail.com>
2022-09-25 19:14:29 +0300

    make sqlcmd output errors with -b option; fix check SQL SERVER availability

--- src/WebApi/db-mssql/import-data.sh
+++ src/WebApi/db-mssql/import-data.sh
@@ -6,7 +6,7 @@ log () {
 # check in a loop if the SQL SERVER is up; because the timing for when the SQL instance is ready is indeterminate
 for i in {1..50};
 do
-  /opt/mssql-tools18/bin/sqlcmd -S localhost -C -U sa -P $(eval echo \$\{MSSQL_SA_PASSWORD\}) -Q "SELECT 1;" -r1 1> NUL
+  /opt/mssql-tools18/bin/sqlcmd -S localhost -C -U sa -P $(eval echo \$\{MSSQL_SA_PASSWORD\}) -Q "SELECT * FROM sys.schemas;" -b
   if [ $? -eq 0 ]
   then
     log "SQL SERVER is ready"
@@ -25,7 +25,7 @@ do
   log "Parsing $x environment variables..."
   envsubst < $x > temp.sql
   log "$x parsed. Executing..."
-  /opt/mssql-tools18/bin/sqlcmd -S localhost -C -U sa -P $(eval echo \$\{MSSQL_SA_PASSWORD\}) -i temp.sql -r1 1> NUL
+  /opt/mssql-tools18/bin/sqlcmd -S localhost -C -U sa -P $(eval echo \$\{MSSQL_SA_PASSWORD\}) -i temp.sql -b
   if [ $? -eq 0 ]
   then
     log "$x executed succesfully"
