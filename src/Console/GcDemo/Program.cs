bc718333ae8a40f8a6a45d1c8d0ee84277481252
RomanGrishin <roman1988grishin@gmail.com>
2022-10-06 19:14:04 +0300

    demo

---
+++ src/Console/GcDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GcDemo;
+
+public class Program
+{
+    private const string EXIT_COMMAND = "exit";
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine($"Type a name, then press  Enter to spawn EventPublisher object, OR type {EXIT_COMMAND} to close app");
+            string input = Console.ReadLine();
+            if (string.Equals(input, EXIT_COMMAND, StringComparison.InvariantCultureIgnoreCase))
+            {
+                break;
+            }
+            EventPublisher.Create(input).SomethingHappened += s => Console.WriteLine(s);
+        }
+    }
+}
